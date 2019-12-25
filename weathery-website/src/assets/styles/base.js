f189a8ca5e8e63329b338362331d7b169a5c565e
Abdo <abdopoetry@gmail.com>
2019-12-26 00:23:08 +0200

    Setup main style base

---
+++ weathery-website/src/assets/styles/base.js
@@ -0,0 +1,32 @@
+// FONT SIZES
+export const fontSizes = {
+  xs: 12,
+  sm: 16,
+  md: 20,
+  lg: 30,
+  huge: 50,
+};
+
+// FONT TYPES
+export const fontTypes = {
+  main: "'Muli', sans-serif",
+};
+
+// COLORS
+const primary = '#ff9d76';
+const secondary = '#333366';
+const black = '#232323';
+const white = '#FFFFFF';
+const red = '#FC4445';
+const gray = '#627273';
+const transparent = '#00FFFF00';
+
+export const colors = {
+  primary,
+  secondary,
+  black,
+  white,
+  red,
+  gray,
+  transparent,
+};
